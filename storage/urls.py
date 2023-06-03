b8ba29ecdd07b8c0a3ae54fbf6f277dc9486e735
Tim_M <stickyxbfexpress16@gmail.com>
2023-06-03 16:04:07 +0300

    design changes and optimization
    +

--- storage/urls.py
+++ storage/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('cart_display', cart_display, name='cart_display'),
     path('item_delete', item_delete),
     re_path(r'^cart/view/update/(?P<requests_id>[0-9]+)$', item_update),
-    re_path(r'^request_check/(?P<sel_list>[a-zA-Z0-9!@#$%^&*\,+]{3,})$', request_check),
-    re_path(r'^request_confirm/(?P<str_list>[a-zA-Z0-9!@#$%^&*\,+]{3,})$', request_confirm),
+    re_path(r'^request_check/(?P<sel_list>[А-Яа-яёЁЇїІіЄєҐґа-щА-ЩЬьЮюЯяЇїІіЄєҐґa-zA-Z0-9!@#$%^&*\,+]{3,})$', request_check),
+    re_path(r'^request_confirm/(?P<str_list>[А-Яа-яёЁЇїІіЄєҐґа-щА-ЩЬьЮюЯяЇїІіЄєҐґa-zA-Z0-9!@#$%^&*\,+]{3,})$', request_confirm),
 ]
 
